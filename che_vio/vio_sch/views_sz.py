8f4700da0722a9dce028f4e93f2453ad820a32b4
barry <lichunbai@outlook.com>
2018-12-30 16:37:09 +0800

    'shenzhou_upload'

--- che_vio/vio_sch/views_sz.py
+++ che_vio/vio_sch/views_sz.py
@@ -723,10 +723,10 @@ def query_vio_auto_sz():
 def backup_vio():
 
     # 数据库连接信息
-    host = 'bj-cdb-gq8xi5ya.sql.tencentcdb.com'
-    port = 63226
-    # host = '172.21.0.2'
-    # port = 3306
+    # host = 'bj-cdb-gq8xi5ya.sql.tencentcdb.com'
+    # port = 63226
+    host = '172.21.0.2'
+    port = 3306
     password = 'Init1234'
     user = 'root'
     database = 'violation'
